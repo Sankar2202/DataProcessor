4022c0cc49b92f04037629eacf06e72521b2038f
Sankar2202 <sankarramasamy22@gmail.com>
2022-04-12 12:05:26 +0530

    Add files via upload
    DataProcessor - UTF8 and Base64

---
+++ ConsoleApp1/FileRead.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace DataProcessor
+{
+    class FileRead
+    {
+        public string FileReading(string filePath, string dataType)
+        {
+            string textFileValue = File.ReadAllText(filePath);
+            switch (dataType)
+            {
+                case "binary":
+                    string val = ToBinary(ConvertToByteArray(textFileValue.Substring(0, 5), Encoding.ASCII));
+                    return Convert.ToBase64String(Encoding.UTF8.GetBytes(val));
+                case "text":
+                    return Encoding.UTF8.GetString(Encoding.Default.GetBytes(textFileValue.Substring(0, 7)));
+                case "textReverse":
+                    return Encoding.UTF8.GetString(Encoding.Default.GetBytes(new string(textFileValue.Reverse().ToArray()).Substring(0, 6)));
+                default:
+                    break;
+            }
+            return textFileValue;
+        }
+        public static byte[] ConvertToByteArray(string str, Encoding encoding)
+        {
+            return encoding.GetBytes(str);
+        }
+        public static String ToBinary(Byte[] data)
+        {
+            return string.Join(" ", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileRead fr = new FileRead();
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string file = Path.Combine(currentDirectory, @"..\..\..\TextFile1.txt");
+            string filePath = Path.GetFullPath(file);
+            Console.WriteLine(fr.FileReading(filePath, "binary"));
+            Console.WriteLine(fr.FileReading(filePath, "text"));
+            Console.WriteLine(fr.FileReading(filePath, "textReverse"));
+        }
+    }
+}
